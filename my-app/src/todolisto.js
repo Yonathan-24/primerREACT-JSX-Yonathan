47843ddd68711c2f9ae110ae5917f5cff527a95b
Your <espinolayona@gmail.com>
2024-06-07 18:21:15 -0400

    Primer commit

---
+++ my-app/src/todolisto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function todolisto() {
+  return (
+    <div>
+      <h2>Lista de tareas:</h2>
+      <ul>
+        <li>Aprender React</li>
+        <li>Practicar JSX</li>
+        <li>Crear componentes</li>
+        <li>Construir una aplicación</li>
+      </ul>
+    </div>
+  );
+}
+
+export default todolisto;
